ae0cd71a4d498f23391f33ecfa9e48e90de87efd
Peter <Peter92@users.noreply.github.com>
2015-11-08 17:03:44 +0000

    Update Movement.py

--- Movement.py
+++ Movement.py
@@ -1,25 +1,34 @@
 class Movement(object):
-    """This was built to allow large coordinates to be stored without causing any floating point precision errors.
-    It is faster than the decimal module, especially with processing large amounts of small movements.
+    """This was built to allow large coordinates to be stored without
+    causing any floating point precision errors.
+    It is faster than the decimal module, especially with processing
+    large amounts of small movements.
     
-    It works by breaking down the coordinates into 'blocks', where each new block is the squared amount of the previous one.
-    The method is very similar to the base number system, in which a block size of 10 will split 235.9 into [5.9, 3, 2].
+    It works by breaking down the coordinates into 'blocks', where each
+    new block is the squared amount of the previous one.
+    The method is very similar to the base number system, in which a
+    block size of 10 will split 235.9 into [5.9, 3, 2].
     
-    A large block size is faster than a small one, though the precision will be worse. At 16 digits, Python can't store any more decimals, so definitely keep it under that.
+    A large block size is faster than a small one, though the precision
+    will be worse. At 16 digits, Python can't store any more decimals,
+    so definitely keep it under that.
     """
     
     BLOCK_SIZE = 65535
     COORDINATES = range(3)
     
     def __init__(self, x=0, y=0, z=0, block_size=None):
-        """Convert the starting coordinates into the format accepted by the class.
+        """Convert the starting coordinates into the format accepted
+        by the class.
         
-        >>> m = Movement('15', '-31564.99933425584842', '1699446367870005.2464800004')
+        >>> m = Movement('15',
+        ...              '-31564.99933425584842',
+        ...              '1699446367870005.2')
         >>> m.player_loc
-        [[15.0], [-31564.99933425585], [38640.2464800004, 17514, 2485, 6]]
+        [[15.0], [-31564.99933425585], [38640.2, 17514, 2485, 6]]
         
         >>> print m
-        (15.0, -31564.9993343, 1699446367870005.24648)
+        (15.0, -31564.9993343, 1699446367870005.2)
         """
         
         #Set a new block size if needed
@@ -27,11 +36,14 @@ class Movement(object):
             self.BLOCK_SIZE = block_size
             
         #Store the initial coordinates
-        self.player_loc = [self.calculate(self._get_integer(i), self._get_decimal(i)) for i in map(str, (x, y, z))]
+        self.player_loc = [self.calculate(self._get_integer(i),
+                                          self._get_decimal(i))
+                           for i in map(str, (x, y, z))]
     
     
     def __repr__(self):
-        """This needs improving, currently it just converts back to the absolute coordinates."""
+        """This needs improving, currently it just converts back to
+        the absolute coordinates."""
         return 'Movement({}, {}, {})'.format(*self._convert_to_world())
     
     
@@ -62,7 +74,8 @@ class Movement(object):
         """
         n = str(n)
         try:
-            self.player_loc[i] = self.calculate(self._get_integer(n), self._get_decimal(n))
+            self.player_loc[i] = self.calculate(self._get_integer(n),
+                                                self._get_decimal(n))
         except IndexError:
             MovementError.index_error_coordinate()
             
@@ -99,7 +112,8 @@ class Movement(object):
         Parameters:
             amount (int/str): Total value without any decimals.
             
-            decimal (int, optional): The decimal value as an integer without the leading '0.'.
+            decimal (int, optional): The decimal value as an integer
+                without the leading '0.'.
         
         >>> Movement().calculate(128)
         [128.0]
@@ -124,8 +138,8 @@ class Movement(object):
             coordinate.append(int(remainder * multiplier))
         coordinate.append(int(amount * multiplier))
         
-        decimal = float('0.' + str(int(decimal))) * int('-1'[not coordinate[0] < 0:])
-        coordinate[0] += decimal
+        decimal = float('0.' + str(int(decimal)))
+        coordinate[0] += decimal * int('-1'[not coordinate[0] < 0:])
         
         return coordinate
 
@@ -136,7 +150,16 @@ class Movement(object):
         Parameters:
             direction (int): Represents X, Y, or Z as a number.
             
-            amount (int or float): Amount to add or subtract from the coordinate.
+            amount (int/float): Amount to add or subtract from the
+            coordinate.
+        
+        >>> m = Movement(135, 426.42, -1499941.5002)
+        >>> print m
+        (135.0, 426.42, -1499941.5002)
+        
+        >>> m.move(100, -5133.100532, 5)
+        >>> print m
+        (235.0, -4706.680532, -1499936.5002)
         """
         
         #Fix to keep decimals on large numbers
@@ -144,19 +167,21 @@ class Movement(object):
             decimal = self.player_loc[direction][0] % 1
             if '.' in str(amount):
                 decimal += float('0.' + str(amount).split('.')[1])
-            self.player_loc[direction][0] = int(self.player_loc[direction][0]) + int(amount)
+            self.player_loc[direction][0] += int(amount)
         else:
             self.player_loc[direction][0] += amount
         
         #Recalculate and add blocks if needed
         i = 0
         while i < len(self.player_loc[direction]):
+            
             stop = True
-            while not -self.BLOCK_SIZE < self.player_loc[direction][i] < self.BLOCK_SIZE:
+            current_block = self.player_loc[direction][i]
+            while not -self.BLOCK_SIZE < current_block < self.BLOCK_SIZE:
                 stop = False
                 
-                remainder = self.player_loc[direction][i] % self.BLOCK_SIZE
-                new_addition = int(self.player_loc[direction][i] - remainder) / self.BLOCK_SIZE
+                remainder = current_block % self.BLOCK_SIZE
+                new_addition = int(current_block - remainder) / self.BLOCK_SIZE
                 if i:
                     remainder = int(remainder)
                 self.player_loc[direction][i] = remainder
@@ -187,11 +212,23 @@ class Movement(object):
     
     
     def _convert_to_world(self):
-        """Convert the blocks into absolute coordinates in string format."""
-        absolute_coordinates = [sum(int(amount) * pow(self.BLOCK_SIZE, i) for i, amount in enumerate(coordinate)) for coordinate in self.player_loc]
-        absolute_coordinates = ['{}.{}'.format(absolute_coordinates[i], str(float(self.player_loc[i][0])).split('.')[1]) for i in self.COORDINATES]
+        """Convert the blocks into absolute coordinates as a string."""
+
+        #Convert coordinates back to numbers, without using floats
+        coordinates = [sum(int(amount) * pow(self.BLOCK_SIZE, i)
+                           for i, amount in enumerate(coordinate))
+                       for coordinate in self.player_loc]
+        
+        #Add the decimal points as strings
+        coordinates = [(str(absolute_coordinates[i])
+                        + '.'
+                        + str(float(self.player_loc[i][0])).split('.')[1]
+                       for i in self.COORDINATES]
+                       
+        #Fix for numbers between -1 and 0
         for i in self.COORDINATES:
-            if len(self.player_loc[i]) == 1 and str(self.player_loc[i][0]).startswith('-0.'):
+            if (len(self.player_loc[i]) == 1
+                and str(self.player_loc[i][0]).startswith('-0.')):
                 absolute_coordinates[i] = '-' + absolute_coordinates[i]
         return absolute_coordinates
             
@@ -201,3 +238,8 @@ class MovementError(Exception):
     @classmethod
     def index_error_coordinate(self):
         raise MovementError('coordinate index out of range')
+        
+        
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
