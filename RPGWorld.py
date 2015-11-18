ecb0a28d4110e54e0a879925c6bb17002cc4d833
Peter <Peter92@users.noreply.github.com>
2015-11-18 22:32:11 +0000

    Create RPGWorld.py

---
+++ RPGWorld.py
@@ -0,0 +1,48 @@
+import pygame
+
+BLACK = (0,   0,   0  )
+BROWN = (153, 76,  0  )
+GREEN = (0,   255, 0  )
+BLUE  = (0,   0,   255)
+WHITE = (255, 255, 255)
+
+
+DIRT = 0
+GRASS = 1
+WATER = 2
+COAL = 3
+
+
+TILECOLOURS = {
+    DIRT  : BROWN,
+    GRASS : GREEN,
+    WATER : BLUE,
+    COAL  : BLACK
+}
+
+#Test case
+tilemap = {
+    (-1, -2): GRASS,
+    ( 0, -2): COAL,
+    ( 1, -2): DIRT,
+    (-1, -1): WATER,
+    ( 0, -1): WATER,
+    ( 1, -1): GRASS,
+    (-1,  0): COAL,
+    ( 0,  0): GRASS,
+    ( 1,  0): WATER,
+    (-1,  1): DIRT,
+    ( 0,  1): GRASS,
+    ( 1,  1): COAL,
+    (-1,  2): GRASS,
+    ( 0,  2): WATER,
+    ( 1,  2): DIRT
+}
+
+#Build a circle
+for x in range(-50, 50):
+    for y in range(-50, 50):
+        if x ** 2 + y ** 2 < 500:
+            tilemap[(x, y)] = GRASS
+
+TILESIZE = 40
