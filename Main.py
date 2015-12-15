62b7ca7e28c20908e2b2e64ace35cc00ee302a72
Peter <Peter92@users.noreply.github.com>
2015-12-15 22:04:44 +0000

    Changed to dungeon keeper style, can mark blocks

--- Main.py
+++ Main.py
@@ -8,137 +8,68 @@ from operator import itemgetter
 from decimal import getcontext, Decimal as d
 _default_precision = getcontext().prec
 
-from RPGCore import Movement, split_decimal_part
-from RPGWorld import *
+from DKWorld import *
+from DKCollection import *
+from FrameLimit import GameTime, GameTimeLoop
 
 def quick_hash(x, y, offset=10):
-    dx = hash(str(x) + str(y // 2))
-    dy = hash(str(y * x // 2))
+    dx = hash(str(x // 2) + str(y))
+    dy = hash(str((y * x) // 2))
     num = int(0.5 * (dx + dy) * (dx + dy + 1)) % (offset * 2) - offset
     return num
-    
 
-class Player(object):
-    """Class to store the player data."""
-
-    def __init__(self, start=(0, 0, 0), bearing=0,
-                 speed_max=1, speed_accel=3, speed_damp=4, 
-                 turn_max=4, turn_accel=16, turn_damp=100000):
-        
-        self.loc = Movement(start)
-        self.bearing = 0
-        
-        self.speed_max = speed_max
-        self.speed_accel = d(speed_accel)
-        self.speed_damp = speed_damp
-        
-        self.turn_max = turn_max
-        self.turn_accel = turn_accel
-        self.turn_damp = turn_damp
-        
-        self.turning = 0
-        self.speed = [d(0) for i in range(3)]
-        self.bearing = bearing
-        self.rz = 0
-        
-        self.has_moved = False
-        self.has_turned = True
-        self.step(1)
+def split_num(n):
+    return (int(n), remove_int(n))
     
-    def _split_speed(self, speed):
-        #Stop if max speed has been reached, around 10^306
-        if str(speed) == 'inf':
-            speed = 0
-        return [d(i) * d(speed) for i in (self.rx, self.ry, self.rz)]
+def remove_int(n):
+    if n > 0:
+        return n % 1
+    try:
+        n_decimal = str(n).split('.')[1]
+        return float('-0.' + n_decimal)
+    except IndexError:
+        return 0.0
+        
+class Camera(object):
+    def __init__(self, x=0, y=0, speed=100):
+        self.x_int, self.x_float = split_num(x)
+        self.y_int, self.y_float = split_num(y)
+        self.speed = speed
     
-    def step(self, frame_time):
-        
-        if not self.has_turned:
-            turn_damp = self.turn_damp * frame_time
-            if self.turning:
-                if self.turning < 0:
-                    self.turning = min(0, self.turning + turn_damp)
-                else:
-                    self.turning = max(0, self.turning - turn_damp)
-                self.has_turned = True
-        
-        if self.has_turned:
-            self.bearing += self.turning * frame_time
-            self.bearing = float(self.bearing) % (2 * math.pi)
-            self.rx = math.sin(self.bearing)
-            self.ry = math.cos(self.bearing)
-            self.has_turned = False
-        
-    
-        frame_time = d(frame_time)
-        if not self.has_moved:
-            speed_damp = self.speed_damp * frame_time
-            for i, speed in enumerate(self.speed):
-                if not speed:
-                    continue
-                elif speed < 0:
-                    self.speed[i] = min(0, speed + speed_damp)
-                else:
-                    self.speed[i] = max(0, speed - speed_damp)
-                self.has_moved = True
-                    
-        if self.has_moved:
-            self.loc.move([i * frame_time for i in self.speed])
-            self.has_moved = False
-        
-        
-    def coordinates(self, update=None):
-        if update:
-            self.loc = Movement(*update)
-        return self.loc._convert_to_world()
-        
-    def turn(self, amount=None, frame_time=1, backwards=False):
-        if amount is None:
-            amount = self.turn_accel
-        if backwards:
-            amount *= -1
-        self.turning += amount * frame_time
-        self.limit_turn()
-        self.has_turned = True
-    
-    def move(self, amount=None, frame_time=1, backwards=False):
-        if amount is None:
-            amount = self.speed_accel
-        if backwards:
-            amount *= -1
-        speed_increment = self._split_speed(amount)
-        
-        #Fix to stop precision running out on super fast speeds
-        current_precision = getcontext().prec
-        str_speed = str(speed_increment[1])
-        len_speed = len(str_speed.split('.')[0])
-        if 'E+' in str_speed:
-            getcontext().prec = int(str_speed.split('E+')[1])
-        elif len_speed > current_precision:
-            getcontext().prec = len_speed
-        elif current_precision != _default_precision and len_speed < _default_precision:
-            getcontext().prec = _default_precision
-            
-        self.speed = [i + j * d(frame_time) for i, j in zip(self.speed, speed_increment)]
-        self.limit_speed()
-        self.has_moved = True
-    
-    def limit_turn(self):
-        if self.turning < 0:
-            self.turning = max(-self.turn_max, self.turning)
+    def _formatstr(self, n_int, n_float):
+        if n_int < 0:
+            n_str = '-'[n_int != -1:] + str(n_int + 1) + '.' + str(1 - n_float)[2:]
         else:
-            self.turning = min(self.turn_max, self.turning)
+            n_str = str(n_int) + '.' + str(n_float)[2:]
+        return n_str
     
-    def limit_speed(self):
-        if self.speed_max is None:
-            return
-        total_speed = d(pow(sum(pow(i, 2) for i in self.speed), d('0.5')))
-        speed_max = d(self.speed_max)
-        if total_speed > speed_max:
-            multiplier = speed_max / total_speed
-            self.speed = [i * multiplier for i in self.speed]
-            
-
+    def __str__(self):
+        return str((self._formatstr(self.x_int, self.x_float), 
+                    self._formatstr(self.y_int, self.y_float))).replace("'", "")
+        
+    def move(self, x=0, y=0, multiplier=1):
+        if x:
+            x_int, x_float = split_num(x * self.speed * multiplier)
+            self.x_int += x_int
+            self.x_float += x_float
+                
+        if y:
+            y_int, y_float = split_num(y * self.speed * multiplier)
+            self.y_int += y_int
+            self.y_float += y_float
+        
+        self.overflow()
+    
+    def overflow(self):
+        i = 1 if self.x_float >= 1 else -1
+        while not 0 <= self.x_float < 1:
+            self.x_float -= i
+            self.x_int += i
+        i = 1 if self.y_float >= 1 else -1
+        while not 0 <= self.y_float < 1:
+            self.y_float -= i
+            self.y_int += i
+    
 class MainGame(object):
 
     WIDTH = 1280
@@ -146,196 +77,172 @@ class MainGame(object):
     DEFAULT_TILE = WATER
     TILE_MAX_SIZE = 128
     TILE_MIN_SIZE = 16
-    
-    def _game_core(self):
-        
-        
-        move_forward = self._keys[pygame.K_w]
-        move_backward = self._keys[pygame.K_s]
-        turn_left = self._keys[pygame.K_a]
-        turn_right = self._keys[pygame.K_d]
-        cam_up = self._keys[pygame.K_UP]
-        cam_down = self._keys[pygame.K_DOWN]
-        cam_left = self._keys[pygame.K_LEFT]
-        cam_right = self._keys[pygame.K_RIGHT]
-        
-        if move_forward:
-            self.player.move(frame_time=self._frame_time)
-        if move_backward:
-            self.player.move(frame_time=self._frame_time, backwards=True)
-        if turn_left:
-            self.player.turn(frame_time=self._frame_time)
-        if turn_right:
-            self.player.turn(frame_time=self._frame_time, backwards=True)
-        
-        n = 1
-        if self._keys[pygame.K_RSHIFT]:
-            n *= sum(len(i) ** (len(i)//2+1) for i in self.camera._convert_to_world())
-        if cam_up:
-            self.camera._move(1, -n)
-        if cam_down:
-            self.camera._move(1, n)
-        if cam_left:
-            self.camera._move(0, -n)
-        if cam_right:
-            self.camera._move(0, n)
-        if any((cam_up, cam_down, cam_left, cam_right)):
-            self.recalculate()
-        
-        self._draw_tiles()
-        
-        self.player.step(self._frame_time)
-        
-        
-    
-    def _draw_tiles(self):
-        
-        self._redraw = True
-        
-        #cam_reversed = [-i for i in self.camera]
-        for x in self.x_range:
-            for y in self.y_range:
-                coordinate = (x, y)
-                try:
-                    colour = TILECOLOURS[tilemap[coordinate]]
-                except:
-                    colour = TILECOLOURS[self.DEFAULT_TILE]
-                    
-                sx, sy = self.block_locations[coordinate]
-                colour_offset = self.block_hashes[coordinate]
-                colour = [min(255, max(0, c + colour_offset)) for c in colour]
-                
-                pygame.draw.rect(self.screen, colour, (sx * self.tilesize, sy * self.tilesize, self.tilesize, self.tilesize)) 
+    FPS = None
+    TICKS = 120
+    BLOCK_TAG = set()
 
-    def _zoom_handler(self):
-        #If the zoom level is changed by anything else, revert it back
+    def recalculate(self):
         try:
-            if self._tilesize_revert is not None:
-                self.tilesize = self._tilesize_revert
+            self.frame_data['Redraw'] = True
         except AttributeError:
             pass
-        if self.tilesize > self.TILE_MAX_SIZE:
-            self.tilesize = self.TILE_MAX_SIZE
-        elif self.tilesize < self.TILE_MIN_SIZE:
-            self.tilesize = self.TILE_MIN_SIZE
-    
-    def recalculate(self):
+        overflow = 1
+        self.x_min = self.cam.x_int - overflow
+        self.y_min = self.cam.y_int - overflow
+        self.x_max = self.cam.x_int + int(self.WIDTH / self.tilesize) + overflow
+        self.y_max = self.cam.y_int + int(self.HEIGHT / self.tilesize) + overflow
         
-        self._zoom_handler()
-    
-        cam_loc = [int(i.split('.')[0]) for i in self.camera._convert_to_world()]
-        self.x_min = cam_loc[0] - 1
-        self.y_min = cam_loc[1] - 1
-        self.x_max = cam_loc[0] + int(self.WIDTH / self.tilesize) + 1
-        self.y_max = cam_loc[1] + int(self.HEIGHT / self.tilesize) + 1
         
-        #To check the culling
-        if False:
-            self.x_min += 1
-            self.x_max -= 1
-            self.y_min += 1
-            self.y_max -= 1
-            
-        self.x_range = range(self.x_min, self.x_max)
-        self.y_range = range(self.y_min, self.y_max)
-        
-        cx, cy = [-int(i.split('.')[0]) for i in self.camera._convert_to_world()]
+        self.screen_coordinates = [(x, y) 
+                                   for x in range(self.x_min, self.x_max) 
+                                   for y in range(self.y_min, self.y_max)]
         
         
         #Delete the keys that have gone off screen
         del_keys = []
-        for key in self.block_locations:
+        for key in self.screen_block_data:
             if not self.x_min < key[0] < self.x_max or not self.y_min < key[1] < self.y_max:
                 del_keys.append(key)
         for key in del_keys:
-            del self.block_locations[key]
-            del self.block_hashes[key]
+            del self.screen_block_data[key]
+        
         
         #Rebuild the new list of blocks
-        for x in self.x_range:
-            for y in self.y_range:
-                coordinate = (x, y)
-                self.block_locations[coordinate] = (x + cx, y + cy)
-                try:
-                    self.block_hashes[coordinate]
-                except KeyError:
-                    self.block_hashes[coordinate] = quick_hash(*coordinate, offset=self.noise_level)
+        block_data_copy = self.screen_block_data.copy()
+        for coordinate in self.screen_coordinates:
+        
+            block_type = get_tile(coordinate)
+            tile_origin = ((coordinate[0] - self.cam.x_int) - self.cam.x_float, 
+                           (coordinate[1] - self.cam.y_int) - self.cam.y_float)
+            tile_location = tuple(i * self.tilesize for i in tile_origin)
+            
+            #Update existing point with new location
+            if coordinate in self.screen_block_data and coordinate not in self.BLOCK_TAG:
+                self.screen_block_data[coordinate][2] = tile_location
+                continue
+            
+            #Generate new point info
+            block_hash = quick_hash(*coordinate, offset=self.noise_level)
+            main_colour = TILECOLOURS[block_type]
+            if coordinate in self.BLOCK_TAG:
+                main_colour = (0, 0, 0)
+            block_colour = [min(255, max(0, c + block_hash)) for c in main_colour]
+            self.screen_block_data[coordinate] = [block_type, 
+                                                  block_colour, 
+                                                  tile_location]
+        
     
-    def _update_window(self):
-        self.block_locations = {}
-        self.block_hashes = {}
+    def setscreen(self):
+        try:
+            self.frame_data['Redraw'] = True
+        except AttributeError:
+            pass
+        self.screen_block_data = {}
+        self.mid_point = [self.WIDTH // 2, self.HEIGHT // 2]
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE | pygame.DOUBLEBUF | pygame.HWSURFACE)
-        self.recalculate()
     
     def play(self):
     
         #Initialise screen
         pygame.init()
-        self.camera = Movement((0, 0))
-        pygame.display.set_caption('Basic RPG')
-        self.clock = pygame.time.Clock()
-        self.tilesize = TILESIZE
-        self.noise_level = 20
+        self.setscreen()
         
-        self._update_window()
+        #Initialise game
+        GT = GameTime(self.FPS, self.TICKS)
+        self.state = 'Main'
+        self.tilesize = TILESIZE
+        self.noise_level = 15
         
         #Fill background
         self.screen.fill((255, 255, 255))
         
-        #Initialise player and movement
-        speed_mult = 100
-        self.player = Player(speed_max=None,#1 * speed_mult, 
-                             speed_accel=3 * speed_mult,  
-                             speed_damp=0,#4 * speed_mult, 
-                             bearing=0)
+        #Camera
+        self.cam = Camera(0, 0, 0.125)
         
-        time_current = time_last = time.time()
-        self.num_frames = 0
-        
+        #Run inital code
+        self.recalculate()
+        self._world_draw()
+        pygame.display.flip()
         while True:
-            self.num_frames += 1
-            time_current = time.time()
-            self._frame_time = time_current - time_last
-            self._events = pygame.event.get()
-            self._keys = pygame.key.get_pressed()
-            self._redraw = False
-            
-            if self._keys[pygame.K_ESCAPE]:
-                pygame.quit()
-            for event in self._events:
-            
-                if event.type == pygame.QUIT:
-                    return
+            with GameTimeLoop(GT) as game_time:
+                self.frame_data = {'Redraw': False,
+                                   'Events': pygame.event.get(),
+                                   'Keys': pygame.key.get_pressed(),
+                                   'MousePos': pygame.mouse.get_pos(),
+                                   'MouseClick': pygame.mouse.get_pressed()}
+                if self.frame_data['Keys'][pygame.K_ESCAPE]:
+                    pygame.quit()
+                for event in self.frame_data['Events']:
+                    if event.type == pygame.QUIT:
+                        return
+                    elif event.type == pygame.VIDEORESIZE:
+                        self.WIDTH, self.HEIGHT = event.dict['size']
+                        self.setscreen()
+                        self.recalculate()
+                #---MAIN LOOP START---
+                
+                if self.state == 'Main':
+                    self.dk_core(game_time.ticks)
+                
+                
+                
+                #---MAIN LOOP END---
+                if game_time.fps:
+                    pygame.display.set_caption('{} {}'.format(game_time.fps, self.cam))
                     
-                elif event.type == pygame.VIDEORESIZE:
-                
-                    self.WIDTH, self.HEIGHT = event.dict['size']
-                    self._update_window()
-                
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    scroll_speed = self.tilesize // 10 + 1
-                    if event.button == 4:
-                        self.tilesize += scroll_speed
-                        self._tilesize_revert = None
-                        self.recalculate()
-                    if event.button == 5:
-                        self.tilesize -= scroll_speed
-                        self._tilesize_revert = None
-                        self.recalculate()
+                if self.frame_data['Redraw']:
+                    self._world_draw()
+                    pygame.display.flip()
+                    self.screen.fill((255, 255, 255))
+    
+    def _world_draw(self):
+        for coordinate in self.screen_coordinates:
+            type, colour, location = self.screen_block_data[coordinate]
+            pygame.draw.rect(self.screen, colour, (location[0], location[1], self.tilesize, self.tilesize))
+    
+    def get_tile_coords(self):
+        click_segment = [split_num(i / self.tilesize) for i in self.frame_data['MousePos']]
+        cam_data = ((self.cam.x_int, self.cam.x_float),
+                    (self.cam.y_int, self.cam.y_float))
+        tile = []
+        for i, num in enumerate(cam_data):
+            new_n_int = num[0] + click_segment[i][0]
+            new_n_float = num[1] + click_segment[i][1]
+            while new_n_float > 1:
+                new_n_int += 1
+                new_n_float -= 1
+            tile.append(new_n_int)
+        return tuple(tile)
+        
+    
+    def dk_core(self, num_ticks):
+        """Multiply values by num_ticks to keep a constant speed."""
+        
+        recalculate = False
+        
+        #Camera control
+        if num_ticks:
+            cam_up = -int(self.frame_data['Keys'][pygame.K_w])
+            cam_down = int(self.frame_data['Keys'][pygame.K_s])
+            cam_left = -int(self.frame_data['Keys'][pygame.K_a])
+            cam_right = int(self.frame_data['Keys'][pygame.K_d])
             
-            self._game_core()
-            #if not self.num_frames % 1000:
-            #    print self.player.loc.raw[1], self.player.speed[1]
+            if cam_up or cam_down or cam_left or cam_right:
+                self.cam.move(cam_left + cam_right, cam_up + cam_down, num_ticks)
+                recalculate = True
             
-            #c = [int(round(float(i))) for i in self.player.coordinates()]
-            #self.screen.set_at((c[0], c[1]), (0, 0, 0))
-            if self._redraw:
-                pygame.display.flip()
-                self.screen.fill((255, 255, 255))
-                #self._draw_tiles()
-                
-            self.clock.tick(60)
-            pygame.display.set_caption(str([self.clock.get_fps(), self.camera._convert_to_world()]))
-            time_last = time_current
+        for event in self.frame_data['Events']:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.BLOCK_TAG.add(self.get_tile_coords())
+                    recalculate = True
+                    
             
+        if recalculate:
+            self.recalculate()
+        
+        
+        if self.frame_data['Keys'][pygame.K_c]:
+            print str(self.cam)
+    
 MainGame().play()
